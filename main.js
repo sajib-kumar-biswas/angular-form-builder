7764ad97b097211a0c55a502b6b686df9dedb4f0
sajib kumar biswas <sajibbiswas01521457841@gmail.com>
2023-05-14 14:46:58 +0600

    added save and open and an icon

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
-const { app, BrowserWindow,Menu,path } = require('electron')
+const { app, BrowserWindow, Menu, ipcMain, dialog } = require('electron')
+const path = require('path')
+const fs = require('fs')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     minWidth: 1000,
     minHeight: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+      preload: path.join(__dirname, 'preload.js'),
+    },
   })
 
   win.setBackgroundColor('rgb(1, 68, 70)');
+
+  const OpenFile = () => {
+    dialog.showOpenDialog({
+      properties: ['openFile']
+    }).then(result => {
+      if (!result.canceled) {
+        fs.readFile(result.filePaths[0], 'utf-8', (err, data) => {
+          if (err) {
+            console.log(err);
+          } else {
+            //console.log('Data read from file: ', data);
+            win.webContents.send('receive-data', data);
+          }
+        })
+      }
+    })
+  }
 
   const template = [
     {
@@ -28,19 +47,47 @@ const createWindow = () => {
       ]
     }
   ]
-  
+
   const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
 
+  //win.webContents.openDevTools()
+
+  ipcMain.handle('save-data', async (event, data) => {
+    dialog.showSaveDialog({
+      title: 'Select the file Path to save',
+      defaultPath: path.join(__dirname, '/assets/sample.txt'),
+      filters: {
+        extensions: ['json']
+      }
+    }).then(result => {
+      if (!result.canceled) {
+        console.log(result.filePath.toString())
+        fs.writeFile(result.filePath.toString(), data[0], (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Data saved to file successfully.");
+          }
+        })
+      }
+    })
+      .catch(err => console.log(err))
+    return "Got the data";
+  })
+
+  ipcMain.on('port', (event) => {
+    const port = event.ports[0];
+    port.on('message', (event) => {
+      console.log("reached")
+      app.relaunch({ args: process.argv.slice(1).concat(['--relaunch']) })
+      app.exit(0)
+    })
+
+    port.start()
+  })
+
   win.loadFile('dist/angular-form-builder/index.html')
-}
-
-const SaveFile = () => {
-
-}
-
-const OpenFile = () => {
-
 }
 
 
--- preload.js
+++ preload.js
@@ -1,5 +1,27 @@
-const { contextBridge, ipcRenderer } = require('electron')
+const { contextBridge, ipcRenderer, app } = require('electron')
 
 ipcRenderer.on('save-file', () => {
-    console.log(window.localStorage.getItem('header'))
+    const beforeUnloadEvent = new Event('beforeunload');
+    window.dispatchEvent(beforeUnloadEvent);
+    let data = {
+        header: JSON.parse(localStorage.getItem('header')),
+        questions: JSON.parse(localStorage.getItem('questions'))
+    }
+
+    data = JSON.stringify(data)
+
+    ipcRenderer.invoke('save-data', [data])
+        .then(result => console.log(result))
 })
+
+ipcRenderer.on('receive-data', (event, data) => {
+    //console.log(data);
+    data = JSON.parse(data);
+    localStorage.setItem('header', JSON.stringify(data.header));
+    localStorage.setItem('questions', JSON.stringify(data.questions));
+    const {port1, port2} = new MessageChannel();
+    port2.postMessage("relaunch");
+    ipcRenderer.postMessage('port', null, [port1]);
+})
+
+console.log("skb")
